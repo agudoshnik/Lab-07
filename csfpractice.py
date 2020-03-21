7b0817870da232234da9062610e358daf751b536
Angelina Gudoshnik <agudoshnik@my.uri.edu>
2020-03-21 13:47:24 -0400

    added python

---
+++ csfpractice.py
@@ -0,0 +1,7 @@
+userName = input("Hello! Please enter your facebook username!")
+userPassword = input("Hello! Please enter your facebook password!")
+if(userName.lower() == "angelina" and userPassword == "M0d!fied"):
+    print("Welcome to facebook!")
+else:
+    print("Wrong credentials, try again!")
+
